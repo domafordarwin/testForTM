80b55ccb480af283350e1b1ff7dc336d7c5459cf
domafordarwin <77951553+domafordarwin@users.noreply.github.com>
2024-01-28 20:07:13 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,49 @@
 import streamlit as st
+import threading
 import cv2
+import numpy as np
+from keras.models import load_model
+from streamlit_webrtc import webrtc_streamer, RTCConfiguration, WebRtcMode
+import av
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
 
-from keras.models import load_model  # TensorFlow is required for Keras to work
+model = load_model('keras_model.h5', compile = False)
+lock = threading.Lock() 
+img_container = {'img':None}
 
-st.title(f'Is it {classes[0]} or {classes[1]}!?')
-img_file_buffer = st.camera_input(f"Take a picture of {classes[0]} or {classes[1]}")
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+    with lock:
+        img_container['img'] = img
+    return frame
 
-if img_file_buffer is not None:
-    bytes_data = img_file_buffer.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-    image = cv2.resize(cv2_img, (224, 224), interpolation=cv2.INTER_AREA)
-    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
-    image = (image / 127.5) - 1
-    probabilities = model.predict(image)
 
-if probabilities[0,0] > 0.8:
-        prob = round(probabilities[0,0] * 100,2)
-        st.write(f"I'm {prob}% sure that's {classes[0]}!")
-    elif probabilities[0,1] > 0.8:
-        prob = round(probabilities[0,1] * 100,2)
-        st.write(f"I'm {prob}% sure that's {classes[1]}!")
-    else:
-        st.write("I'm not confident that I know what this is! ")
+st.title('Is it Pikachu or Eevee!?')
+st.subheader('The image detection tool you definitely do not need in your life')
 
-st.balloons()
+ctx = webrtc_streamer(key="example", 
+                video_frame_callback=video_frame_callback,
+                rtc_configuration = RTC_CONFIGURATION,
+                mode=WebRtcMode.SENDRECV)
+st.write('Pikachu')
+pikachu = st.progress(0)
+st.write('Eevee')
+eevee = st.progress(0)
+
+
+while ctx.state.playing:
+    with lock: 
+        img = img_container['img']
+    if img is None:
+        continue
+    img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_AREA)
+    img = np.asarray(img, dtype=np.float32).reshape(1, 224, 224, 3)
+    img = (img / 127.5) - 1
+    probabilities = model.predict(img)
+    
+    pikachu_p = int(probabilities[0,0] * 100)
+    eevee_p = int(probabilities[0,1] * 100)
+    pikachu.progress(pikachu_p)
+    eevee.progress(eevee_p)
